28a77f8e681c70893920c0f95a53c7040af8644e
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-05-31 09:52:13 -0700

    flat buttons

--- lib/main.dart
+++ lib/main.dart
@@ -22,27 +22,20 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Center(
-          child : Text(
-            "Hi Am James learning Dart",
-            textAlign: TextAlign.center,
-            textDirection: TextDirection.rtl,
-            softWrap: true,
-            overflow: TextOverflow.visible,
-            style: TextStyle(
-              fontSize: 40.0,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic,
-              //color: Colors.blue,
-              color:Color(0xffe74c3c),
-              //color: Color.fromRGBO(255, 0, 0, 1.0),
-              fontFamily: "Lucida Sans Unicode",
-              decoration: TextDecoration.underline,
-              decorationColor: Colors.black,
-              decorationStyle: TextDecorationStyle.dotted,
-              //background: Paint()..color= Colors.black..style = PaintingStyle.stroke,
-             // backgroundColor: Colors.black
+          child : FlatButton(
+            onPressed:() {},
+            color: Colors.red,
+           // highlightColor: Colors.cyan,
+            splashColor: Colors.green,
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+            child: Text(
+              "flat button",
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+              ),
             ),
-          ),
+        ),
       ),
       ),
     );
