c482df002e6208421128adc0847763211ac3aea0
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-06-02 12:13:30 -0700

    working with images

--- lib/main.dart
+++ lib/main.dart
@@ -6,89 +6,17 @@ void main(){
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title : "Sample App",
-      theme : ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home : Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "My App",
-            style: TextStyle(
-              fontSize: 20.0,
-            ),
-          ),
-        ),
-        body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "this is text",
-              style:TextStyle(
-                color: Colors.red,
-                fontSize: 20.0,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-            ),
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Image.asset("assets/images/rice.jpg",
+        height:100.0,
+        width:100.0,
+          fit:BoxFit.cover,
 
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                    onPressed: () {},
-                    elevation: 30.0,
-                    color: Colors.white,
-                    shape: Border.all(color: Colors.black, width: 2.0, style: BorderStyle.solid),
-                    child: Text(
-                      "raised button 1",
-                      softWrap: true,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                      ),
-                    )
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                ),
-                RaisedButton(
-                    onPressed: () {},
-                    elevation: 30.0,
-                    color: Colors.white,
-                    shape: Border.all(color: Colors.black, width: 2.0, style: BorderStyle.solid),
-                    child: Text(
-                      "raised button 2",
-                      softWrap: true,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                      ),
-                    )
-                ),
-          ]
         ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-            ),
-            Text(
-              "this is text",
-              style:TextStyle(
-                color: Colors.red,
-                fontSize: 20.0,
-              ),
-            ),
-        ],
+        //Image.network("url"), image url
       ),
-      ),
-      ),
-      );
-
+    );
   }
 }
