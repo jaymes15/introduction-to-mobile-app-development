1c7e83e7ec9775cf04f894a7835d3a8515ae7efe
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-06-07 10:33:37 -0700

    material splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:async';
 
 void main(){
   runApp(MyApp());
@@ -8,10 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Text Field",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: "Material splash",
       home: MyHomepage(),
     );
   }
@@ -23,82 +22,46 @@ class MyHomepage extends StatefulWidget{
 }
 
 class _MyHomepageState extends State<MyHomepage>{
+@override
+void initState(){
+  super.initState();
+  debugPrint("started The splash screen");
+  Timer(Duration(seconds: 10),finished);
+}
+String display="";
+void finished(){
+  debugPrint("Finished");
+  setState(() {
+    display = "Splash \n Screen";
+  });
 
+}
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text('App Bar'),
+      backgroundColor: Colors.red,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(
+                backgroundColor: Colors.white,
+              ),
+                Text(
+                  display,
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontFamily: "cursive",
+                  ),
+                ),
+            ],
+          ),
+        ],
       ),
-      //bottomSheet:Text("this is a bottom sheet "),
-//      floatingActionButton: IconButton(
-//        onPressed:(){},
-//        icon: Icon(Icons.message),
-//        color: Colors.green,
-//      ),
-//      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: SizedBox(
-                height: 60.0,
-                child: InkWell(
-                  onTap: (){},
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.search,
-                      ),
-                      Text("search"),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-
-
-            Expanded(
-              child: SizedBox(
-                height: 60.0,
-                child: InkWell(
-                  onTap: (){},
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.search,
-                      ),
-                      Text("search"),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-
-
-            Expanded(
-              child: SizedBox(
-                height: 60.0,
-                child: InkWell(
-                  onTap: (){},
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.search,
-                      ),
-                      Text("search"),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        )
-      ),
-      body: Center(),
     );
   }
 }
